cfeac868cd7778fb3c85a7596c552e1e952bcfda
gconnect <agatevureglory@gmail.com>
2019-09-26 11:41:23 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from send_mail import send_mail
+
+
+app = Flask(__name__)
+
+ENV = 'prod'
+# development
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:quickerpay@localhost/feedback_db'
+else:
+    # production
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://avmwehxerlpfgv:015708fac78a9c8976a19fcd8cd3b20e5174b876e29be855966f0b4d6d3208af@ec2-54-83-55-122.compute-1.amazonaws.com:5432/d5i1jioovf89ca'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# database object
+db = SQLAlchemy(app)
+
+
+# Database model
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200), unique=True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+
+# Feedback Constructor
+def __init__(self, customer, dealer, rating, comments):
+    self.customer = customer
+    self.dealer = dealer
+    self.rating = rating
+    self.comments = comments
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        customer = request.form['customer']
+        dealer = request.form['dealer']
+        rating = request.form['rating']
+        comments = request.form['comments']
+        if customer == "" or dealer == "":
+            return render_template('index.html', message="Please enter required fields")
+        # print(customer, dealer, rating, comments)
+        if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
+            data = Feedback(customer= customer, dealer =dealer, rating=rating, comments= comments)
+            db.session.add(data)
+            db.session.commit()
+            send_mail(customer, dealer, rating, comments)
+            return render_template('success.html')
+        return render_template('index.html', message="You have already submitted a feedback")
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ send_mail.py
@@ -0,0 +1,20 @@
+import smtplib
+from email.mime.text import MIMEText
+
+def send_mail(customer, dealer, rating, comments):
+    port=255
+    smtp_server = 'smtp.mailtrap.io'
+    username = 'e18b0a5be4374c'
+    password = '8b9e56dd0b2d45'
+    message = f"<h3> New Feedback Submission</h3> <ul><li>Customer: {customer} </li> <li>Dealer: {dealer} </li><li>Rating: {rating} </li><li>Comments: {comments}</li></ul>"
+    sender_email = 'email1@example.com'
+    receiver_email = 'email2@example.com'
+    msg = MIMEText(message, 'html')
+    msg['subject'] = 'Payowners Feedback Form'
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+
+    #Send email
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
